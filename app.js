a1369e40f09c8f5b3c52dd7eb18f5181e96d7113
user name <98519572+OkolieOKS234@users.noreply.github.com>
2022-08-17 23:34:13 +0100

    Added new things in my ejs file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/userDB");
-console.log(process.env.SECRET);
+
 const userSchema = new mongoose.Schema({
   firstname: String,
   lastname: String,
